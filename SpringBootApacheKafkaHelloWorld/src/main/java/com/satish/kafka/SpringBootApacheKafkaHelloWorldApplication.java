bb851d083ee12507cfa8ffc22abeded7fea13236
Satish Prasad <spd61185@gmail.com>
2020-07-08 16:34:47 +0530

    SpringBootApacheKafkaHelloWorld

---
+++ SpringBootApacheKafkaHelloWorld/src/main/java/com/satish/kafka/SpringBootApacheKafkaHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.satish.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApacheKafkaHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApacheKafkaHelloWorldApplication.class, args);
+	}
+
+}
---
+++ SpringBootApacheKafkaHelloWorld/src/test/java/com/satish/kafka/SpringBootApacheKafkaHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satish.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApacheKafkaHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
